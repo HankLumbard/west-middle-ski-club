41adaa0c5c2eff08fa5c6b21cdede63d5026bb19
HankLumbard <158756999+HankLumbard@users.noreply.github.com>
2026-09-23 09:58:54 -0400

    Connect ski club registration form to Google Sheet

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Paste the deployed Apps Script Web app /exec URL here before accepting signups.
+// Google Apps Script Web app URL for ski club registrations.
 window.SKI_CLUB_CONFIG = Object.freeze({
-  scriptUrl: "",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbyR9ZO-g4xEYUoB5jxXkcQqpK9eZC_WQtpCeKBToV65H3IASy59zacQNZgQ5OZiOfW0nQ/exec",
   venmoUsername: "Henry-Lumbard-1",
   cardPrice: 45
 });
