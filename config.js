45c558628d739546f2c7e647217d800de0c5202c
HankLumbard <158756999+HankLumbard@users.noreply.github.com>
2026-09-23 11:33:06 -0400

    Use Sheet-managed prices

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Google Apps Script Web app URL for ski club registrations.
 window.SKI_CLUB_CONFIG = Object.freeze({
   scriptUrl: "https://script.google.com/macros/s/AKfycbyR9ZO-g4xEYUoB5jxXkcQqpK9eZC_WQtpCeKBToV65H3IASy59zacQNZgQ5OZiOfW0nQ/exec",
-  venmoUsername: "Henry-Lumbard-1",
-  cardPrice: 45
+  venmoUsername: "Henry-Lumbard-1"
 });
+
